5c97a28b80e3e8ab099cd5bcbb95469227868804
SkavenLord58 <adamkrasa@sd58.cz>
2024-12-13 19:48:29 +0100

    FIX: autogreeter formatting

--- main.py
+++ main.py
@@ -62,10 +62,10 @@ async def batch_react(m, reactions: List):
 async def on_member_join(member: disnake.Member):
     welcome_channel = client.get_channel(decdi.WELCOMEPERO)
     await welcome_channel.send(f"""
-                         Vítej, {member.mention}!
-                         Prosím, přesuň se do <#1314388851304955904> a naklikej si role. Nezapomeň na roli Člen, abys viděl i ostatní kanály!
-                         ---
-                         Please, go to the <#1314388851304955904> channel and select your roles. Don't forget the 'Člen'/Member role to see other channels!
+Vítej, {member.mention}!
+Prosím, přesuň se do <#1314388851304955904> a naklikej si role. Nezapomeň na roli Člen, abys viděl i ostatní kanály!
+---
+Please, go to the <#1314388851304955904> channel and select your roles. Don't forget the 'Člen'/Member role to see other channels!
                         """)
     pass
 
